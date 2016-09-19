1e1f2239809bc7d16202fa3bdadda270728afcb5
Vibeesh Bose <vtbose@gmail.com>
2016-09-18 21:47:40 -0500

    Followup commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LogarithmicSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random(25);
+            
+            for (var i = 1; i < 100; ++i)
+            {
+                var value = r.Next(1, 100000);
+                Console.WriteLine(i + " - " + value+ " - " +i+" - "+ Math.Log10(value));
+            }
+        }
+    }
+}
